097ab1bef1d0cd0e3d7c99f4723339c0c6242437
nikhil03a <105121377+nikhil03a@users.noreply.github.com>
2022-08-17 21:40:58 +0530

    Update 142_Linked_List_Cycle_II.java

--- 142_Linked_List_Cycle_II.java
+++ 142_Linked_List_Cycle_II.java
@@ -1,1 +1,47 @@
-
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        int len =  length(head);
+        ListNode f = head;
+        ListNode s = head;
+        if(len==0){
+            return null;
+        }
+        while(len>0){
+            s=s.next;
+            len--;
+        }
+        while(f!=s){
+            f=f.next;
+            s=s.next;
+        }
+        return f;
+    }
+    public int length(ListNode head){
+        ListNode f = head;
+        ListNode s = head;
+        int ans=0;
+        while(f!=null && f.next!=null){
+            f=f.next.next;
+            s=s.next;
+            if(f==s){
+                do{
+                    f=f.next;
+                    ans++;
+                }while(f!=s);
+                return ans;
+            }
+        }
+        return ans;
+    }
+}
