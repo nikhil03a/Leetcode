050dead308a139d94fb00595d0b072ecfc86b0f7
nikhil03a <105121377+nikhil03a@users.noreply.github.com>
2022-08-27 22:12:03 +0530

    Time: 5 ms (37.55%), Space: 188.5 MB (12.16%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -19,10 +19,8 @@ class Solution {
             node1  = node1.next;
         }
         node1 = head;
-        ListNode prev = null;
         ListNode node2 = head;
         for(int i=2;i<=k;i++){
-            prev = node1;
             node1 = node1.next;
         }
         int n = size-k+1;
@@ -32,21 +30,6 @@ class Solution {
         int temp = node1.val;
         node1.val = node2.val;
         node2.val = temp;
-        // ListNode con = prev;
-        // ListNode tail = node1;
-        // ListNode pre = node1;
-        // ListNode curr = node1.next;
-        // while(pre!=node2 && curr!=null){
-        //     ListNode rem = curr.next;
-        //     curr.next = pre;
-        //     pre = curr;
-        //     curr = rem;
-        // }
-        // tail.next = curr;
-        // if(con!=null)
-        //     con.next = pre;
-        // else
-        //     head = pre;
         return head;
     }
 }
