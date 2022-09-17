9d47a05482b33c72f1fb44a3923894f22233203e
nikhil03a <105121377+nikhil03a@users.noreply.github.com>
2022-09-17 10:17:20 +0530

    Time: 1 ms (58.84%), Space: 44 MB (39.36%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,21 +14,29 @@
  * }
  */
 class Solution {
-    List<Integer> list = new ArrayList<Integer>();
+    // List<Integer> list = new ArrayList<Integer>();
     public boolean isValidBST(TreeNode root) {
-        inorder(root);
-        for(int i=0; i<list.size()-1; i++){
-            if(list.get(i+1)<=list.get(i)){
-                return false;
-            }
-        }
-        return true;
+        // inorder(root);
+        // for(int i=0; i<list.size()-1; i++){
+        //     if(list.get(i+1)<=list.get(i)){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        return dfs(root, Long.MAX_VALUE, Long.MIN_VALUE);
     }
-    public void inorder(TreeNode root){
+    // public void inorder(TreeNode root){
+    //     if(root==null)
+    //         return;
+    //     inorder(root.left);
+    //     list.add(root.val);
+    //     inorder(root.right);       
+    // }
+    public boolean dfs(TreeNode root, long max, long min){
         if(root==null)
-            return;
-        inorder(root.left);
-        list.add(root.val);
-        inorder(root.right);       
+            return true;
+        if(!(root.val<max && root.val>min))
+            return false;
+        return dfs(root.left,root.val, min) && dfs(root.right, max, root.val);
     }
 }
